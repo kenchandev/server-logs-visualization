a7f199b1dab5fdb383173f2c26a5e27f75aac2da
Ken Chan <KChan923@gmail.com>
2015-08-05 13:58:09 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,75 @@
+var fs = require('fs');
+var http = require('http');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var socketIO = require('socket.io');
+
+var index = require('./routes/index');
+
+var app = express();
+//  Must attach socket.io to an http server instance.
+//  http://stackoverflow.com/questions/13499010/nodejs-express-launching-my-app-express-createserver-is-deprecated
+var server = http.createServer(app);
+var io = socketIO(server);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+io.on('connection', function(socket){
+  //  While the client is connected.
+
+  socket.on('watch', function(obj){
+
+  });
+
+  socket.on('disconnect', function(){
+    //  When the client disconnects.
+  })
+});
+
+module.exports = app;
